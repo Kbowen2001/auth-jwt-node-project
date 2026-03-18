313fbee277bbf2f680406d3364638fb66201b431
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-18 12:17:19 -0600

    adding

--- routes/db/connect.js
+++ routes/db/connect.js
@@ -1,4 +1,58 @@
-const mongoose = require("mongoose");
+
+
+
+const dontenv = require("dotenv");
+dontenv.config();
+
+const MongoClient = require("mongodb").MongoClient;
+
+let _db;
+
+const initDb = async (callback) => {
+  if (_db) {
+    console.log("Db is already initialized!");
+    return callback(null, _db);
+  }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+        _db = client.db();
+        callback(null, _db);
+    })
+    .catch((err) => {
+        callback(err);
+    });
+};
+
+const getDb = () => {
+    if (!_db) {
+        throw Error("Db not initialized");
+    }
+    return _db;
+};
+
+module.exports = {
+    initDb,
+    getDb,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	const mongoUri = process.env.MONGO_URI || process.env.MONGODB_URI;
@@ -8,3 +62,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+*/
