e710e9e4989dae0c9e78998c76309527fecf5429
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-18 12:23:53 -0600

    ADDING TO SERVER.JS

--- server.js
+++ server.js
@@ -1,4 +1,50 @@
-require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongodb = require("./db/connect");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors()).use("/", require ("./routes"));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT);
+    console.log (
+      "\x1b[34m%s\x1b[0m",
+      `connected to DB and listening on ${PORT} `
+    );
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** require("dotenv").config();
 const express = require("express");
 const connectDB = require("./routes/db/connect");
 const app = express();
@@ -19,3 +65,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+*/
