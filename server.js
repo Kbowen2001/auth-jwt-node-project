3092234c060e8ba060f7c8cd050809ea33272368
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-18 12:40:44 -0600

    ad

--- server.js
+++ server.js
@@ -44,26 +44,3 @@ mongodb.initDb((err) => {
 
 
 
-/** require("dotenv").config();
-const express = require("express");
-const connectDB = require("./routes/db/connect");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use("/", require("./routes"));
-
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`Test server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
-  }
-};
-
-startServer();
-
-*/
