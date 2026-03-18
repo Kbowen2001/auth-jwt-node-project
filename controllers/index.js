12b10d5839384fb100eb8d87f5e1a9d60ae0956f
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-18 12:51:38 -0600

    adding to controller index file

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,33 @@
-const awesomeFunction = (Req, res) => {
+
+const mongodb = require("../db/connect");
+const objectId = require("mongodb").ObjectId;
+
+const awesomeFunction = (req, res) => {
+  res.send("Awesome Name!");
+};
+
+const tooeleTechFunction = (req, res) => {
+  res.send("Tooele Tech is Awesome!");
+};
+
+const getAllStudents =  async (req, res) => {
+  try{
+    const result = await mongodb.getDb().db().collection("students").find();
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  }
+   catch (err) {
+    res.status(500).json(error);
+  }
+};
+
+
+
+
+
+/** const awesomeFunction = (Req, res) => {
   res.send("Hello World!");
 };
 
@@ -7,4 +36,4 @@ const tooeleTech = (Req, res) => {
 };
 
 module.exports = { awesomeFunction, tooeleTech };
- 
+*/ 
