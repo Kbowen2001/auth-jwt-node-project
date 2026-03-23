86a7eeaa63d650e9c6d9c2fa888bc8c72601ed87
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-23 10:03:52 -0600

    adding to controllers index to get a single contact

--- controllers/index.js
+++ controllers/index.js
@@ -24,6 +24,25 @@ const getAllStudents =  async (req, res) => {
 };
  
 
+//GET single contact
+
+const getSingleStudent = async (req, res) => {
+  try {
+    const userID = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("students")
+      .find({_id: userId});
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
 
 module.exports = {
   awesomeFunction,
