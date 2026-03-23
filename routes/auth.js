88632b05d72dca5dd47515af97ec87f7c36c546b
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-23 11:58:58 -0600

    create auth.js and add code

---
+++ routes/auth.js
@@ -0,0 +1,1 @@
+const express = require("express"); const router = express.Router(); const authController = require("../controllers/authController"); router.get("/home", authController.home); module.exports = router;
